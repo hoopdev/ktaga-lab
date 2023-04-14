bdf82b38ad23e72601d77ae518168502e506f876
ktaga <taga.kotaro.62d@st.kyoto-u.ac.jp>
2023-04-14 16:06:30 +0900

    Fix N5224A bugs

--- ktaga_lab/drivers/Keysight/Keysight_VNA.py
+++ ktaga_lab/drivers/Keysight/Keysight_VNA.py
@@ -854,6 +854,390 @@ class PNABase(VisaInstrument):
             port._set_power_limits(min_power, max_power)
 
 
+class PNABaseMinimum(VisaInstrument):
+    """
+    Base qcodes driver for Agilent/Keysight series PNAs
+    http://na.support.keysight.com/pna/help/latest/Programming/GP-IB_Command_Finder/SCPI_Command_Tree.htm
+    Note: Currently this driver only expects a single channel on the PNA. We
+        can handle multiple traces, but using traces across multiple channels
+        may have unexpected results.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        address: str,
+        # Set frequency ranges
+        min_freq: Union[int, float],
+        max_freq: Union[int, float],
+        # Set power ranges
+        min_power: Union[int, float],
+        max_power: Union[int, float],
+        nports: int,  # Number of ports on the PNA
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(name, address, terminator="\n", **kwargs)
+        self.min_freq = min_freq
+        self.max_freq = max_freq
+
+        self.log.info(
+            "Initializing %s with power range %r-%r, freq range %r-%r.",
+            name,
+            min_power,
+            max_power,
+            min_freq,
+            max_freq,
+        )
+
+        # Ports
+        ports = ChannelList(self, "PNAPorts", PNAPort)
+        for port_num in range(1, nports + 1):
+            port = PNAPort(
+                self, f"port{port_num}", port_num, min_power, max_power
+            )
+            ports.append(port)
+            self.add_submodule(f"port{port_num}", port)
+        self.add_submodule("ports", ports.to_channel_tuple())
+
+        # RF output
+        self.add_parameter(
+            "output",
+            label="RF Output",
+            get_cmd=":OUTPut?",
+            set_cmd=":OUTPut {}",
+            val_mapping=create_on_off_val_mapping(on_val="1", off_val="0"),
+        )
+
+        # Drive power
+        self.add_parameter(
+            "power",
+            label="Power",
+            get_cmd="SOUR:POW?",
+            get_parser=float,
+            set_cmd="SOUR:POW {:.2f}",
+            unit="dBm",
+            vals=Numbers(min_value=min_power, max_value=max_power),
+        )
+
+        # IF bandwidth
+        self.add_parameter(
+            "if_bandwidth",
+            label="IF Bandwidth",
+            get_cmd="SENS:BAND?",
+            get_parser=float,
+            set_cmd="SENS:BAND {:.2f}",
+            unit="Hz",
+            vals=Numbers(min_value=1, max_value=15e6),
+        )
+
+        # Number of averages (also resets averages)
+        self.add_parameter(
+            "averages_enabled",
+            label="Averages Enabled",
+            get_cmd="SENS:AVER?",
+            set_cmd="SENS:AVER {}",
+            val_mapping={True: "1", False: "0"},
+        )
+        self.add_parameter(
+            "averages",
+            label="Averages",
+            get_cmd="SENS:AVER:COUN?",
+            get_parser=int,
+            set_cmd="SENS:AVER:COUN {:d}",
+            unit="",
+            vals=Numbers(min_value=1, max_value=65536),
+        )
+
+        # Setting frequency range
+        self.add_parameter(
+            "start",
+            label="Start Frequency",
+            get_cmd="SENS:FREQ:STAR?",
+            get_parser=float,
+            set_cmd="SENS:FREQ:STAR {}",
+            unit="Hz",
+            vals=Numbers(min_value=min_freq, max_value=max_freq),
+        )
+        self.add_parameter(
+            "stop",
+            label="Stop Frequency",
+            get_cmd="SENS:FREQ:STOP?",
+            get_parser=float,
+            set_cmd="SENS:FREQ:STOP {}",
+            unit="Hz",
+            vals=Numbers(min_value=min_freq, max_value=max_freq),
+        )
+        self.add_parameter(
+            "center",
+            label="Center Frequency",
+            get_cmd="SENS:FREQ:CENT?",
+            get_parser=float,
+            set_cmd="SENS:FREQ:CENT {}",
+            unit="Hz",
+            vals=Numbers(min_value=min_freq, max_value=max_freq),
+        )
+        self.add_parameter(
+            "span",
+            label="Frequency Span",
+            get_cmd="SENS:FREQ:SPAN?",
+            get_parser=float,
+            set_cmd="SENS:FREQ:SPAN {}",
+            unit="Hz",
+            vals=Numbers(min_value=min_freq, max_value=max_freq),
+        )
+        self.add_parameter(
+            "cw",
+            label="CW Frequency",
+            get_cmd="SENS:FREQ:CW?",
+            get_parser=float,
+            set_cmd="SENS:FREQ:CW {}",
+            unit="Hz",
+            vals=Numbers(min_value=min_freq, max_value=max_freq),
+        )
+
+        # Number of points in a sweep
+        self.add_parameter(
+            "points",
+            label="Points",
+            get_cmd="SENS:SWE:POIN?",
+            get_parser=int,
+            set_cmd="SENS:SWE:POIN {}",
+            unit="",
+            vals=Numbers(min_value=1, max_value=100001),
+        )
+
+        # Sweep Time
+        self.add_parameter(
+            "sweep_time",
+            label="Time",
+            get_cmd="SENS:SWE:TIME?",
+            get_parser=float,
+            unit="s",
+            vals=Numbers(0, 1e6),
+        )
+        # Sweep Mode
+        self.add_parameter(
+            "sweep_mode",
+            label="Mode",
+            get_cmd="SENS:SWE:MODE?",
+            set_cmd="SENS:SWE:MODE {}",
+            vals=Enum("HOLD", "CONT", "GRO", "SING"),
+        )
+        # Sweep Type
+        self.add_parameter(
+            "sweep_type",
+            label="Type",
+            get_cmd="SENS:SWE:TYPE?",
+            set_cmd="SENS:SWE:TYPE {}",
+            vals=Enum("LIN", "LOG", "POW", "CW", "SEGM", "PHAS"),
+        )
+
+        # Group trigger count
+        self.add_parameter(
+            "group_trigger_count",
+            get_cmd="SENS:SWE:GRO:COUN?",
+            get_parser=int,
+            set_cmd="SENS:SWE:GRO:COUN {}",
+            vals=Ints(1, 2000000),
+        )
+        # Trigger Source
+        self.add_parameter(
+            "trigger_source",
+            get_cmd="TRIG:SOUR?",
+            set_cmd="TRIG:SOUR {}",
+            vals=Enum("EXT", "IMM", "MAN"),
+        )
+
+        # Axis Parameters
+        self.add_parameter(
+            "frequency_axis",
+            unit="Hz",
+            label="Frequency",
+            parameter_class=PNAAxisParameter,
+            startparam=self.start,
+            stopparam=self.stop,
+            pointsparam=self.points,
+            vals=Arrays(shape=(self.points,)),
+        )
+        self.add_parameter(
+            "frequency_log_axis",
+            unit="Hz",
+            label="Frequency",
+            parameter_class=PNALogAxisParamter,
+            startparam=self.start,
+            stopparam=self.stop,
+            pointsparam=self.points,
+            vals=Arrays(shape=(self.points,)),
+        )
+        self.add_parameter(
+            "time_axis",
+            unit="s",
+            label="Time",
+            parameter_class=PNATimeAxisParameter,
+            startparam=None,
+            stopparam=self.sweep_time,
+            pointsparam=self.points,
+            vals=Arrays(shape=(self.points,)),
+        )
+        self.add_parameter(
+            "time_domain_axis",
+            unit="s",
+            label="Time",
+            parameter_class=PNATimeDomainAxisParameter,
+            startparam=self.start_time,
+            stopparam=self.stop_time,
+            pointsparam=self.points,
+            vals=Arrays(shape=(self.points,)),
+        )
+
+        # Traces
+        self.add_parameter(
+            "active_trace",
+            label="Active Trace",
+            get_cmd="CALC:PAR:MNUM?",
+            get_parser=int,
+            set_cmd="CALC:PAR:SEL 'Meas{}'",
+            vals=Numbers(min_value=1, max_value=24),
+        )
+        # Note: Traces will be accessed through the traces property which
+        # updates the channellist to include only active trace numbers
+        self._traces = ChannelList(self, "PNATraces", PNATrace)
+        self.add_submodule("traces", self._traces)
+        # Add shortcuts to first trace
+        trace1 = self.traces[0]
+        params = trace1.parameters
+        if not isinstance(params, dict):
+            raise RuntimeError(
+                f"Expected trace.parameters to be a dict got "
+                f"{type(params)}"
+            )
+        for param in params.values():
+            self.parameters[param.name] = param
+        # And also add a link to run sweep
+        self.run_sweep = trace1.run_sweep
+        # Set this trace to be the default (it's possible to end up in a
+        # situation where no traces are selected, causing parameter snapshots
+        # to fail)
+        self.active_trace(trace1.trace_num)
+
+        # Set auto_sweep parameter
+        # If we want to return multiple traces per setpoint without sweeping
+        # multiple times, we should set this to false
+        self.add_parameter(
+            "auto_sweep",
+            label="Auto Sweep",
+            set_cmd=None,
+            get_cmd=None,
+            vals=Bool(),
+            initial_value=True,
+        )
+
+        # A default output format on initialisation
+        self.write("FORM REAL,32")
+        self.write("FORM:BORD NORM")
+
+        self.connect_message()
+
+    @property
+    def traces(self) -> ChannelList:
+        """
+        Update channel list with active traces and return the new list
+        """
+        # Keep track of which trace was active before. This command may fail
+        # if no traces were selected.
+        try:
+            active_trace = self.active_trace()
+        except errors.VisaIOError as e:
+            self.log.debug("Exception on querying active trace: %r", e)
+            if e.error_code == constants.StatusCode.error_timeout:
+                self.log.info("No active trace on PNA")
+                active_trace = None
+            else:
+                raise
+
+        # Get a list of traces from the instrument and fill in the traces list
+        parlist = self.get_trace_catalog().split(",")
+        self._traces.clear()
+        for trace_name in parlist[::2]:
+            trace_num = self.select_trace_by_name(trace_name)
+            pna_trace = PNATrace(self, f"tr{trace_num}", trace_name, trace_num)
+            self._traces.append(pna_trace)
+
+        # Restore the active trace if there was one
+        if active_trace:
+            self.active_trace(active_trace)
+
+        # Return the list of traces on the instrument
+        return self._traces
+
+    def get_options(self) -> Sequence[str]:
+        # Query the instrument for what options are installed
+        return self.ask("*OPT?").strip('"').split(",")
+
+    def preset(self) -> None:
+        self.write("SYST:FPRESET")
+        self.write("DISP:WIND:STATE ON")
+
+    def get_trace_catalog(self) -> str:
+        """
+        Get the trace catalog, that is a list of trace and sweep types
+        from the PNA.
+        The format of the returned trace is:
+            trace_name,trace_type,trace_name,trace_type...
+        """
+        return self.ask("CALC:PAR:CAT:EXT?").strip('"')
+
+    def select_trace_by_name(self, trace_name: str) -> int:
+        """
+        Select a trace on the PNA by name.
+        Returns:
+            The trace number of the selected trace
+        """
+        self.write(f"CALC:PAR:SEL '{trace_name}'")
+        return self.active_trace()
+
+    def add_trace(self, trace_num: int, trace_meas: str) -> None:
+        """
+        Add a trace to the channel.
+        """
+        self.write(f"CALC:PAR:DEF:EXT 'Meas{trace_num}',{trace_meas}")
+        self.write(f"DISP:WIND:TRACe{trace_num}:FEED 'Meas{trace_num}'")
+
+    def reset_averages(self) -> None:
+        """
+        Reset averaging
+        """
+        self.write("SENS:AVER:CLE")
+
+    def averages_on(self) -> None:
+        """
+        Turn on trace averaging
+        """
+        self.averages_enabled(True)
+
+    def averages_off(self) -> None:
+        """
+        Turn off trace averaging
+        """
+        self.averages_enabled(False)
+
+    def autoscale(self) -> None:
+        """
+        Autoscale windows
+        """
+        self.write("DISP:WIND:Y:AUTO")
+
+    def _set_power_limits(
+        self, min_power: Union[int, float], max_power: Union[int, float]
+    ) -> None:
+        """
+        Set port power limits
+        """
+        self.power.vals = Numbers(min_value=min_power, max_value=max_power)
+        for port in self.ports:
+            port._set_power_limits(min_power, max_power)
+
+
 class P9375A(PNABase):
     def __init__(self, name: str, address: str, **kwargs: Any):
         super().__init__(
@@ -882,7 +1266,7 @@ class E8362B(PNABase):
         )
 
 
-class N5224A(PNABase):
+class N5224A(PNABaseMinimum):
     def __init__(self, name: str, address: str, **kwargs: Any):
         super().__init__(
             name,
